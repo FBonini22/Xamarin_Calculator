a61945f312cd3da1962d7daf4af311dd7b5b1869
Frank Bonini <frankb5000@gmail.com>
2020-02-17 19:23:19 -0500

    Begin working on calculation classes. Added: Calculator; CalculatorExpression

---
+++ Xamarin_Calculator/Xamarin_Calculator/Calculator.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin_Calculator
+{
+    /// <summary>
+    /// A class which is contains various functions used to calculate things.
+    /// </summary>
+    public class Calculator
+    {
+        //CONSTANTS
+
+
+
+        //INSTANCE VARIABLES
+        private double lastCalculatedValue = 0f;
+
+        /// <summary>
+        /// Default constructor. Currently, this class requires no parameters.
+        /// </summary>
+        public Calculator()
+        {
+
+        }
+
+
+        public double Calculate(CalculatorExpression exp)
+        {
+            
+            switch (exp.Operator)
+            {
+                case "/":
+                    break;
+                case "+":
+                    break;
+                case "-":
+                    break;
+                case "*":
+                    break;
+            }
+        }
+
+
+
+        private double Add(double operand1, double operand2)
+        {
+            double sum = operand1 + operand2;
+
+
+        }
+
+
+        //Accessors
+        /// <summary>
+        /// Gets the delimiter used to parse <see cref="Calculator"/> input strings.
+        /// </summary>
+        /// <returns>Returns the delimiter as a string. This should be one character.</returns>
+        public string GetDelimiter()
+        {
+            return DELIMITER;
+        }
+    }
+}
